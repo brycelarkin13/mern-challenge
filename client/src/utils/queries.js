2c0fb82039e66112fd92ce46a82960156ed5f17d
bryce <blarkin13@gmail.com>
2022-05-22 19:43:03 -0500

    import apollo provider

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import gql from 'apollo-server-express';
+
+export const GET_ME = gql`
+{
+    me{
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            description
+            title
+            image
+            link
+        }
+    }
+}`;
